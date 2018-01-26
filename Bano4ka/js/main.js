8ca78e5b339ec1b86fec57e9611e5dc48f387876
Dmitry_scroll <31383143+Dimc4ik@users.noreply.github.com>
2018-01-27 01:16:11 +0200

    beta
    Its to close to origin version

---
+++ Bano4ka/js/main.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+            $(".button1").click(function () {
+                $(".button2").hide()
+                $(".button3").hide()
+                $(".button4").hide()
+                $(".button5").hide()
+                $(".button6").hide()
+                $(".button7").hide()
+            });
+            $(".button2").click(function () {
+                $(".button1").hide();
+                $(".button3").hide();
+                $(".button4").hide();
+                $(".button5").hide();
+                $(".button6").hide();
+                $(".button7").hide();
+            });
+            $(".button3").click(function () {
+                $(".button1").hide();
+                $(".button2").hide();
+                $(".button4").hide();
+                $(".button5").hide();
+                $(".button6").hide();
+                $(".button7").hide();
+            });
+            $(".button4").click(function () {
+                $(".button1").hide();
+                $(".button2").hide();
+                $(".button3").hide();
+                $(".button5").hide();
+                $(".button6").hide();
+                $(".button7").hide();
+            });
+            $(".button5").click(function () {
+                $(".button1").hide();
+                $(".button2").hide();
+                $(".button3").hide();
+                $(".button4").hide();
+                $(".button6").hide();
+                $(".button7").hide();
+            });
+            $(".button6").click(function () {
+                $(".button1").hide();
+                $(".button2").hide();
+                $(".button3").hide();
+                $(".button4").hide();
+                $(".button5").hide();
+                $(".button7").hide();
+            });
+            $(".button7").click(function () {
+                $(".button1").hide();
+                $(".button2").hide();
+                $(".button3").hide();
+                $(".button4").hide();
+                $(".button5").hide();
+                $(".button6").hide();
+            });
+ 
+        });
