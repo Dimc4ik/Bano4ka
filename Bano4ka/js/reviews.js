5a17564d6fcc78d6c9b396e21fb7404bb1be5612
Dmitry_scroll <31383143+Dimc4ik@users.noreply.github.com>
2018-01-30 00:12:48 +0200

    30/11

---
+++ Bano4ka/js/reviews.js
@@ -0,0 +1,11 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    animateOut: true,
+    smartSpeed:2000,
+    items: 2,
+    loop: true,
+    dots: true
+});
